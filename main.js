1ad60655a209436546896f1a615b2776bef63e97
Alejandro Dominguez <adborroto90@gmail.com>
2025-12-06 20:46:42 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, globalShortcut } = require("electron");
+
+let win = null;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 520,
+    height: 680,
+    frame: false,
+    alwaysOnTop: true,
+    roundedCorners: true,
+    vibrancy: "sidebar",
+    webPreferences: {
+      preload: __dirname + "/preload.js",
+    },
+  });
+
+  win.loadURL("https://gemini.google.com/app");
+
+  win.on("blur", () => {
+    win.hide();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // Toggle window con CMD + SHIFT + SPACE
+  globalShortcut.register("CommandOrControl+Shift+Space", () => {
+    if (win.isVisible()) {
+      win.hide();
+    } else {
+      win.show();
+      win.focus();
+    }
+  });
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("will-quit", () => {
+  globalShortcut.unregisterAll();
+});
